a5158f1c10f0568586a0ca0bc0a52b825a6a6b81
FoxRefire <155989196+FoxRefire@users.noreply.github.com>
2024-09-01 04:54:58 +0900

    Add more supported file types

--- utils/cleanup.js
+++ utils/cleanup.js
@@ -11,7 +11,9 @@ export default function(file){
             [cleanImages, [".jpg", ".jpeg", ".png", ".gif", ".bmp", ".webp"]],
             [cleanPdf, [".pdf"]],
             [cleanOffice, [".doc", ".dot", ".docx", ".dotx", ".docm", ".dotm", ".xls", ".xlt", ".xla", ".xlsx", ".xltx", ".xlsm", ".xltm", ".xlam", ".xlsb", ".ppt", ".pot", ".pps", ".ppa", ".pptx", ".potx", ".ppsx", ".ppam", ".pptm", ".potm", ".ppsm", ".mdb", ".odt", ".ods", ".odp", ".odg", ".odc", ".odf", ".odi", ".odm", ".odb", ".ott", ".ots", ".otp", ".otg", ".otc", ".oti", ".oth"]],
-            [cleanByFFmpeg, [".mp4", ".webm", ".ogv", ".mpg", ".mpeg", ".m1v", ".m4v", ".avi", ".mkv", ".mov", ".wmv"]]
+            [cleanByFFmpeg, [".mp4", ".webm", ".ogv", ".mpg", ".mpeg", ".m1v", ".m4v", ".avi", ".mkv", ".mov", ".wmv", ".avif", ".avifs"]], //Videos
+            [cleanByFFmpeg, [".mp3", ".aac", ".wav", ".wv", ".ogg", ".opus", ".flac"]], //Audios
+            [cleanByFFmpeg,[".heic", ".ppm", ".tiff"]] //Minor images
         ]
 
         for(let util of utils){
